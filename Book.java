18bbd7c4de2927843744d22a235b50dbaa8bc8eb
AbbasFaizi <abbasfaizi.af@gmail.com>
2023-04-05 22:07:33 +0200

    made a book class to represents a book in the inventory

---
+++ Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    private String title;
+    private String author;
+    private int quantity;
+    private double price;
+
+    public Book(String title, String author, int quantity, double price) {
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s by %s, %d in stock, $%.2f each", title, author, quantity, price);
+    }
+}
